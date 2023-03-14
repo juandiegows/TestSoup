36f01aa6d58fb6c748c48bcb38d0dc4f76616df1
Juan Diego Mejía Maestre <mejiamaestrejuandiego@gmail.com>
2023-03-14 18:48:18 -0500

    subir ultimo cambio

--- TestSoup/Controllers/TestController.cs
+++ TestSoup/Controllers/TestController.cs
@@ -13,56 +13,33 @@ using System.Security.Authentication;
 using System.Net.Security;
 using System.IO;
 using System.Reflection.PortableExecutable;
+using System.Net;
+using Microsoft.Web.Services3;
+using System.Xml;
+using System.Security.Cryptography.Xml;
+using System.Security.Cryptography;
 
 namespace TestSoup.Controllers {
     [Route("api/[controller]")]
     [ApiController]
     public class TestController : ControllerBase {
+        String certPath = Path.Combine("wwwroot", "ssl", "MetlifeTESTJK1.pfx");
+        String password = "MetlifeTESTJK1";
+        //string text = System.IO.File.ReadAllText(certPath.ToString());
+        //return StatusCode(200, text);
+        string username = "273562";
+        string pass = "GspBuro2023#";
+        String url = "https://miportafoliouat.transunion.co/ws/UbicaPlusWebService/services/UbicaPlus";
+
         [HttpGet]
         public async Task<ActionResult> Index() {
 
-
-            var certPath = Path.Combine("wwwroot", "ssl", "MetlifeTESTJK1.pfx");
-            var password = "MetlifeTESTJK1";
-            //string text = System.IO.File.ReadAllText(certPath.ToString());
-            //return StatusCode(200, text);
-            string username = "273562";
-            string pass = "GspBuro2023#";
-            var url = "https://miportafoliouat.transunion.co/ws/UbicaPlusWebService/services/UbicaPlus";
-
-            Envelope data = new Envelope() {
-                Body = new Body() {
-                    consultaUbicaPlus = new ConsultaUbicaPlus() {
-                        parametrosUbica = new ParametrosUbica() {
-                            motivoConsulta = "24",
-                            codigoInformacion = "5632",
-                            numeroIdentificacion = "17809529",
-                            primerApellido = "JIMENEZ",
-                            tipoIdentificacion = "1"
-                        }
-                    }
-                },
-                Header = ""
-            };
-
-
-            var serializer = new XmlSerializer(typeof(Envelope));
-            var stringWriter = new StringWriter();
-
-            // Serializamos el objeto a XML y lo escribimos en el StringWriter
-            serializer.Serialize(stringWriter, data);
-
-            // Convertimos el StringWriter a un string
-            var xmlData = stringWriter.ToString();
             try {
 
                 var cert = new X509Certificate2(certPath, password);
                 cert.FriendlyName = "MetlifeTESTJK1";
-                var handler = new HttpClientHandler();
 
-            
-                //var sslContext = new SslStream(cert, false, (sender, certificate, chain, errors) => true); // Establece la cadena de certificados
-                //handler.SslStreamFactory = () => sslContext;
+                var handler = new HttpClientHandler();
 
                 handler.ClientCertificates.Add(cert);
 
@@ -74,47 +51,98 @@ namespace TestSoup.Controllers {
                 var authInfoEncoded =
                     Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
 
-                //return StatusCode(200, authInfoEncoded);
                 client.DefaultRequestHeaders.Add("SOAPAction", url);
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authInfoEncoded);
                 client.DefaultRequestHeaders.TransferEncoding.Add(new TransferCodingHeaderValue("chunked"));
 
-                string headertext = "";
-                foreach (var item in client.DefaultRequestHeaders) {
-                    headertext += $"{item.Key}: {string.Join(", ", item.Value)}\n";
-                }
+                //string headertext = "";
+                //foreach (var item in client.DefaultRequestHeaders) {
+                //    headertext += $"{item.Key}: {string.Join(", ", item.Value)}\n";
+                //}
 
-                //return StatusCode(200, headertext);
+                ////return StatusCode(200, headertext);
 
-                var content = new StringContent(GetXmlString(), Encoding.UTF8, "application/xml");
+                // Crea un objeto XmlDocument con el contenido de la solicitud SOAP
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(GetXmlString());
+
+                SignedXml signedXml = new SignedXml(xmlDoc);
+                signedXml.SigningKey = cert.PrivateKey;
+                Reference reference = new Reference("#body");
+
+                signedXml.AddReference(reference);
+
+                KeyInfo keyInfo = new KeyInfo();
+                keyInfo.AddClause(new KeyInfoX509Data(cert));
+                signedXml.KeyInfo = keyInfo;
+                signedXml.ComputeSignature();
+
+                XmlElement signatureElement = signedXml.GetXml();
+                XmlElement soapHeader = (XmlElement)(xmlDoc.GetElementsByTagName("soapenv:Header")[0]);
+                soapHeader.AppendChild(signatureElement);
+
+
+                var content = new StringContent(xmlDoc.OuterXml, Encoding.UTF8, "application/xml");
 
                 var response = await client.PostAsync(url, content);
                 var content2 = await response.Content.ReadAsStringAsync();
          
-                return StatusCode((int)response.StatusCode, content2);
+                return StatusCode((int)response.StatusCode, xmlDoc.OuterXml);
 
             }
             catch (Exception ex) {
 
                 return BadRequest(ex.Message + " => " + certPath);
             }
-            return Ok("HOla");
+        }
+
+        [HttpGet("login")]
+        public ActionResult Login() {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "text/xml;charset=UTF-8";
+
+            NetworkCredential credentials = new NetworkCredential(username, pass);
+            request.Credentials = credentials;
+
+            string authInfo = credentials.UserName + ":" + credentials.Password;
+            authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
+            request.Headers["Authorization"] = "Basic " + authInfo;
+            XmlDocument soapEnvelopeXml = new XmlDocument();
+            soapEnvelopeXml.LoadXml(GetXmlString());
+
+            var cert = new X509Certificate2(certPath, password);
+            cert.FriendlyName = "MetlifeTESTJK1";
+            request.ClientCertificates.Add(cert);
+
+            using (Stream stream = request.GetRequestStream()) {
+                soapEnvelopeXml.Save(stream);
+            }
+
+
+            try {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse()) {
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream())) {
+                        string responseXml = reader.ReadToEnd();
+                        // procesa la respuesta aquí
+                        return StatusCode((int)response.StatusCode, responseXml);
+                    }
+                }
+            }
+            catch (Exception err) {
+
+                return BadRequest(err.Message);
+            }
+            return Ok();
         }
 
         private string GetXmlString() {
             string xmlString = @"<soapenv:Envelope xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:ubic=""http://ubicaplus.webservice.com"">
- <soapenv:Header>
-<wsse:Security xmlns:wsse=""http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"" xmlns:wsu=""http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"">
-<wsu:Timestamp wsu:Id=""TS-7ECF631BE8AE9E5B09167752860077852"">
-<wsu:Created>2023-03-11T19:10:00.778Z</wsu:Created>
-<wsu:Expires>2023-03-27T22:56:40.778Z</wsu:Expires>
-</wsu:Timestamp>
+  <soapenv:Header >
+  </soapenv:Header>
 
 
-</wsse:Security>
-</soapenv:Header>
-
-   <soapenv:Body>
+   <soapenv:Body id=""body"" >
       <ubic:consultaUbicaPlus soapenv:encodingStyle=""http://schemas.xmlsoap.org/soap/encoding/"">
          <parametrosUbica xsi:type=""dto:ParametrosUbicaPlusDTO"" xmlns:dto=""http://dto.ubicaplus.webservice.com"">
             <codigoInformacion xsi:type=""soapenc:string"" xmlns:soapenc=""http://schemas.xmlsoap.org/soap/encoding/"">5632</codigoInformacion>
@@ -129,5 +157,6 @@ namespace TestSoup.Controllers {
 
             return xmlString;
         }
+
     }
 }
